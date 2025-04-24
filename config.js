6f55d653406c26895a0cf443c90e836950be8d5e
temu569 <thembinkosichipere@gmail.com>
2025-04-25 00:24:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ DEBUG =
 module.exports = {
   VERSION: "v1.2.8",
   SESSION:
-    process.env.ASENA_SESSION === undefined ? "" : process.env.ASENA_SESSION,
+    process.env.ASENA_SESSION === undefined ? "levanter_25fb4189480d0d4afc896f7497c63947ab" : process.env.ASENA_SESSION,
   EXT: process.env.EXT === undefined ? undefined : process.env.EXT,
   LANG:
     process.env.LANGUAGE === undefined
